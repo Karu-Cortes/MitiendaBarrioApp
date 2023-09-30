7fa08411abcefa2fc869bdde0517b86b78e6c658
KarenCortes <karenj.cortesn@gmail.com>
2023-09-30 11:17:20 -0500

    menú de opciones

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,9 @@
 package org.example;
+import java.util.Scanner;
 
 public class Main {
+
+
     public static void main(String[] args) {
         System.out.println(" ");
         System.out.println("Creación de Mi Tienda De Barrio App");
@@ -20,12 +23,62 @@ public class Main {
 
         System.out.println(" ");
         System.out.println("Nombre del producto: " + nombreProducto);
-        System.out.println("Descripción de la producto: " +tipoEmpaque+ " " +medida+ " " +tipoMedida+ " ");
-        System.out.println("Categoria del producto: " +categoria );
-        System.out.println("Etiqueta del producto: " +etiqueta);
-        System.out.println("Cantidad disponible del producto: " +cantidad);
-        System.out.println("Precio del producto: " +precio);
-        System.out.println("Foto del producto: " +urlFoto);
+        System.out.println("Descripción de la producto: " + tipoEmpaque + " " + medida + " " + tipoMedida + " ");
+        System.out.println("Categoria del producto: " + categoria);
+        System.out.println("Etiqueta del producto: " + etiqueta);
+        System.out.println("Cantidad disponible del producto: " + cantidad);
+        System.out.println("Precio del producto: " + precio);
+        System.out.println("Foto del producto: " + urlFoto);
         System.out.println("+++++++++++++++++++++++++++++++++++++++++++++");
+
+        int opcion;
+        do {
+            opcion = construirMenu();
+            ejecutarOpcion(opcion);
+        } while (opcion != 5);
     }
-}
+
+
+
+    public static int construirMenu() {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("** Menú de opciones para inventario **");
+        System.out.println("1. Listar productos");
+        System.out.println("2. Agregar producto");
+        System.out.println("3. Modificar producto");
+        System.out.println("4. Eliminar producto");
+        System.out.println("5. Salir");
+        System.out.print("Ingrese una opción: ");
+        return sc.nextInt();
+    }
+
+    public static void ejecutarOpcion(int opcion) {
+        switch (opcion) {
+            case 1:
+                System.out.println("Listando productos...");
+                break;
+            case 2:
+                System.out.println("Agregando producto...");
+                break;
+            case 3:
+                System.out.println("Modificando producto...");
+                break;
+            case 4:
+                System.out.println("Eliminando producto...");
+                break;
+            case 5:
+                System.out.println("Saliendo del menú...");
+                break;
+            default:
+                System.out.println("Opción no válida.");
+                break;
+        }
+        }
+
+    }
+
+
+
+
